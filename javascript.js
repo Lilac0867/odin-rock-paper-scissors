a14e856f3a7ecfcea2ab57fe6ea40066f9660dd4
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-06-19 07:27:40 -0500

    Edit javascript.js to add getHumanChoice

--- javascript.js
+++ javascript.js
@@ -12,4 +12,20 @@ function getComputerChoice() {
     return ans;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let isTrue = true;
+    let ans = "";
+
+    while(isTrue) {
+        ans = prompt("Please select rock, paper, or scissors: ").toLowerCase().trim();
+        if(ans !== "rock" && ans !== "paper" && ans !== "scissors") {
+            console.log(`'${ans}' is not rock, paper, or scissors. Please try again.`);
+        } else {
+            isTrue = false;
+        }
+    }
+
+    return ans;
+}
+
+console.log(getHumanChoice());
