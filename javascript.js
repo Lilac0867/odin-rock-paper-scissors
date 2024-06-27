bb257b8c04730cda42876d74cbb409a223b9ef78
Lilac0867 <116083750+Lilac0867@users.noreply.github.com>
2024-06-27 06:37:21 -0500

    Edit javascript.js to add playRound

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const num = Math.floor((Math.random() * 100) / 33.3) + 1;
     let ans = "";
@@ -28,4 +31,40 @@ function getHumanChoice() {
     return ans;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) { // Start here!
+    let winner = "You ";
+    if(humanChoice === 'rock') {
+        if(computerChoice === 'paper') {
+            computerScore++;
+            winner += "lose! Paper beats Rock.";
+        } else if(computerChoice === 'scissors') {
+            humanScore++;
+            winner += "win! Rock beats Scissors.";
+        } else {
+            winner += "tie! Both players chose Rock.";
+        }
+    } else if(humanChoice === 'paper') {
+        if(computerChoice === 'scissors') {
+            computerScore++;
+            winner += "lose! Scissors beat Paper.";
+        } else if(computerChoice === 'rock') {
+            humanScore++;
+            winner += "win! Paper beats Rock.";
+        } else {
+            winner += "tie! Both players chose Paper.";
+        }
+    } else {
+        if(computerChoice === 'rock') {
+            computerScore++;
+            winner += "lose! Rock beats Scissors.";
+        } else if(computerChoice === 'paper') {
+            humanScore++;
+            winner += "win! Scissors beat Paper.";
+        } else {
+            winner += "tie! Both players chose Scissors.";
+        }
+    }
+    return winner;
+}
+
+alert(playRound(getHumanChoice(), getComputerChoice()));
